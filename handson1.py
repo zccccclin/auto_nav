15c02cb9f590a79311288dcab36fb4bd7ea960f1
jinggadavid <jinggadavid@gmail.com>
2020-02-04 18:29:00 +0700

    handson 1

---
+++ handson1.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb  4 17:43:19 2020
+
+@author: notfromajedi
+"""
+
+import time
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+
+test_point = 
+
+GPIO.setup(test_point,GPIO.OUT)
+
+try:
+    while True:
+        GPIO.output(test_point,GPIO.HIGH)
+        time.sleep(1)
+        GPIO.output(test_point,GPIO.LOW)
+        time.sleep(1)
+except KeyboardInterrupt:
+    GPIO.cleanup()
+    
+    
