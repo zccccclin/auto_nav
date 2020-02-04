19febee419726df3fa4f438c68bbfec3dd6fdb67
burger <e0406517@u.nus.edu>
2020-02-04 05:24:33 -0800

    git

--- servo2.py
+++ servo2.py
@@ -7,16 +7,17 @@ GPIO.setup(servo_pin,GPIO.OUT)
 p=GPIO.PWM(servo_pin,50)
 p.start(7.5)
 
-def serv():
-    a = int(input("Enter degree: "))
-    deg = (a/180)*12.5
-    p.ChangeDutyCycle(deg)
-    time.sleep(1)
-
 try:
     while True:
-        serv()
-        
+	a = input("Enter degree between 0 and 180: ")
+	if a<0 or a>180:
+		a = input("Enter degree between 0 and 180: ")
+	deg = round((a*10)/180,3) + 2.500
+	print(deg)
+	p.ChangeDutyCycle(deg)
+	time.sleep(1)
+	print("Rotated "+str(a)+" degrees")
+
 except KeyboardInterrupt:
     p.stop()
     GPIO.cleanup()
