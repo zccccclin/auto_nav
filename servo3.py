969d00c1a53caf336e4819c6b198fc71a19f7cbb
turtlebot2 <e0406517@u.nus.edu>
2020-02-05 03:02:16 -0800

    Updated function

--- servo3.py
+++ servo3.py
@@ -5,7 +5,16 @@ GPIO.setmode(GPIO.BOARD)
 servo_pin=32
 GPIO.setup(servo_pin,GPIO.OUT)
 p=GPIO.PWM(servo_pin,50)
-p.start(7.5)
+#p.start(7.5)
+
+def serv1():
+    a = float(input("Enter degree between 0 and 180: "))
+    while a<0 or a>180:
+        a = float(input("Enter degree between 0 and 180: "))
+    deg =round( a*10/180,5) + 2.5
+    p.start(deg)
+    print("Rotated to "+str(a)+" degrees")
+
 
 def serv():
     a = float(input("Enter degree between 0 and 180: "))
@@ -16,6 +25,7 @@ def serv():
 #    time.sleep(1)
     print("Rotated to "+str(a)+" degrees")
 
+serv1()
 
 try:
     while True:
