a1e5fbe24b39cc950cac0a1061d4a0e4f1b99fce
zccccclin <60165401+zccccclin@users.noreply.github.com>
2020-02-10 12:13:46 +0800

    Update trial1.py

--- trial1.py
+++ trial1.py
@@ -9,12 +9,9 @@ GPIO.setup(test_point,GPIO.OUT)
 
 try:
     while True:
-        a = str(input('Enter command: open/close ')
-        if a == 'open':
-            GPIO.output(test_point,GPIO.HIGH)
-            time.sleep(1)
-        elif a == 'close':
-            GPIO.output(test_point,GPIO.LOW)
-            time.sleep(1)
+        GPIO.output(test_point,GPIO.HIGH)
+        time.sleep(1)
+        GPIO.output(test_point,GPIO.LOW)
+        time.sleep(1)
 except KeyboardInterrupt:
     GPIO.cleanup()
