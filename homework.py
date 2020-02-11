f61367b4d26a847c3cec44e6a171ad2c91673a8f
zccccclin <e0406517@u.nus.edu>
2020-02-11 13:09:56 +0800

    y

--- homework.py
+++ homework.py
@@ -7,40 +7,61 @@ import time
 import RPi.GPIO as GPIO
 
 
-GPIO.setmode(GPIO.BOARD)
-servo = 32
-plunger = 36
-
-GPIO.setup(servo, GPIO.OUT)
-GPIO.setup(plunger, GPIO.OUT)
-p=GPIO.PWM(servo,50)
-p.start(7.5)
+servo = 32 #servo_pin
+plunger = 36 #plunger_pin
+angle = 2.5 #45 degrees angle for servo
 laser_range = np.array([])
+p=GPIO.PWM(servo,50)
 
 
-def getlaser(msg):
+
+def servo_setup(): #setup servo
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(servo, GPIO.OUT)
+    global p
+    p.start(7.5)
+    
+def plunger_setup(): #setup plunger
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(plunger, GPIO.OUT)
+    
+def rotation(): #function to rotate servo 45 degrees
+    global p
+    global angle
+    angle *= -1
+    p.ChangeDutyCycle(7.5+angle)
+    time.sleep(1)
+
+def plunger_punch(): #function to punch plunger 1 time
+    GPIO.output(plunger,GPIO.HIGH)
+    time.sleep(0.1)
+    GPIO.output(plunger,GPIO.LOW)
+    time.sleep(0.1)
+    
+def getlaser(msg): #read lidar data as array
     	global laser_range
         laser_range = np.array([msg.ranges])
 	
-
-
 def scanner():
     global laser_range
 	# initialize node
     rospy.init_node('scanner', anonymous=True)
 
 	# set the update rate to 1 Hz
-    rate = rospy.Rate(5) # 1 Hz
+    rate = rospy.Rate(1) # 1 Hz
 
 	# subscribe to LaserScan data
     rospy.Subscriber('scan', LaserScan, getlaser)
-    lr2 = laser_range
-    lr2i = lr2[0][0]
-    rospy.loginfo(lr2i)
-        
-	# wait until it is time to run again
-    rate.sleep()
-
+    
+    servo_setup()
+    plunger_setup()
+    while True:
+        lr2 = laser_range[0]
+        lr2i = lr2[0]
+        if round(lr2i,5) == 1:
+            rotation()
+            plunger_punch()
+        rate.sleep()
 	# spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
 
